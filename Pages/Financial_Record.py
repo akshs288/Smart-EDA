928afffe3aa808b4615b7e2d29b183fbe434517b
akshs288 <aksht098@gmail.com>
2026-08-12 23:15:11 +0530

    Adding categorical summ

--- Pages/Financial_Record.py
+++ Pages/Financial_Record.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-st.subheader("Numerical Analysis")
+st.subheader("📈Numerical Analysis")
 
 if "df" in st.session_state:
     df = st.session_state["df"]
+    summary_data = df.describe()
+    st.write(summary_data)
 
+    st.subheader("Categorical Summary")
+    
 
 
 
