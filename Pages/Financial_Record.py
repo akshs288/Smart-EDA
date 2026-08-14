5651392dce6a903f3df269f6caa311ee67296dcd
akshs288 <aksht098@gmail.com>
2026-08-14 21:26:16 +0530

    Most frequent val

--- Pages/Financial_Record.py
+++ Pages/Financial_Record.py
@@ -10,6 +10,14 @@ if "df" in st.session_state:
 
     st.subheader("Categorical Summary")
     
+    cola,colb= st.columns(2)
+
+    with cola:
+        list_names = df.select_dtypes(include=["object"]).columns
+        first_col_name = list_names[1]
+        max_occ = df[first_col_name].mode()[0]
+
+        st.metric(f"🔁Most Frequent '{first_col_name}' value",max_occ,border=True)
 
 
 
