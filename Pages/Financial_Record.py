5cf1f6d95d620d21ee829bb9a5ee2b2df67f1bfd
akshs288 <aksht098@gmail.com>
2026-08-15 21:21:07 +0530

    Least frequent value used

--- Pages/Financial_Record.py
+++ Pages/Financial_Record.py
@@ -12,12 +12,16 @@ if "df" in st.session_state:
     
     cola,colb= st.columns(2)
 
+    list_names = df.select_dtypes(include=["object"]).columns
     with cola:
-        list_names = df.select_dtypes(include=["object"]).columns
         first_col_name = list_names[1]
         max_occ = df[first_col_name].mode()[0]
 
         st.metric(f"🔁Most Frequent '{first_col_name}' value",max_occ,border=True)
 
+    with colb:
+        first_col_name2 = list_names[2]
+        min_occ = df[first_col_name2].value_counts().sort_values(ascending=False).iloc[2]
+        st.metric(f"🔁Least Frequent '{first_col_name2}' value",min_occ,border=True)
 
 
