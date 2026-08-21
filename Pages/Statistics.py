6f6b8b6d522861dc7be44066bb1f9226694ff3e4
akshs288 <aksht098@gmail.com>
2026-08-21 20:13:30 +0530

    Correaltion cases added

--- Pages/Statistics.py
+++ Pages/Statistics.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import pandas as pd
+import numpy as np
+
 
 st.subheader("📊Statistical Analysis")
 
@@ -17,14 +18,28 @@ if "df" in st.session_state:
             else:
                 new_lst.append(i)                
 
-        df[new_lst] = df[new_lst].fillna(df[new_lst].mean())
-        corr_matrix = df[new_lst].corr()
-        print(corr_matrix)
-        
+        new_df = df[new_lst].copy()
+        new_df = new_df.fillna(new_df.mean())
+        corr_matrix = new_df[new_lst].corr()
 
+        st.subheader("🎯Correlation Matrix")
+        
+        upper = corr_matrix.where(
+        np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+        # Strongest positive
+        max_pair = upper.stack().idxmax()
+        max_value = upper.stack().max()
+
+        # Strongest negative
+        min_pair = upper.stack().idxmin()
+        min_value = upper.stack().min()
+        
+        highest_corr = f"{max_pair[0]} <-> {max_pair[1]} = {max_value}"
+        st.metric(f"📈Highest Correlation:",highest_corr,border=True)
+        
+        lowest_corr = f"{min_pair[0]} <-> {min_pair[1]} = {min_value}"
+        st.metric(f"📉Lowest Correlation:",lowest_corr,border=True)
+        
+        
     else:
         st.error("This data does not fit to show correlation and covariance tables.")
-
-
-
-
